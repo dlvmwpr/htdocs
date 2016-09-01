b8bf86130dc0bd64784c24c6136a531ac4390d36
dlvmwpr <dlvmwpr>
2016-09-01 10:26:03 +0900

    HTML에 PHP를 넣자

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+</head>
+<body>
+<ul>
+    <li>1번</li>
+    <li>
+    <?php
+        print "ㅁㅇㄻㄴㅇㄻㄴ";
+    ?>
+    </li>
+    <li>2번</li>
+    <li>3번</li>
+    <li>4번</li>
+</ul>
+</body>
+</html>
