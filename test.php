88aa755ba39b8333f305198570a17241172de748
dlvmwpr <dlvmwpr>
2016-09-01 11:34:18 +0900

    for

--- test.php
+++ test.php
@@ -6,6 +6,10 @@
 <head>
     <meta charset="UTF-8">
     <title>php</title>
+    <style>
+        .gugudan { background:lime;}
+        .odddan { background: white;}
+    </style>
 </head>
 <body>
 <ul>
@@ -26,6 +30,51 @@
     ?>
     </li>
     <li>4번</li>
-</ul>
-</body>
+    <?php
+        $data = "안녕하세요";
+        $data1 = 1;
+        print $data;
+        print $data1;
+        $week[] = "월";
+        $week[] = "화";
+    
+        $week[0] = "월";
+        $week[1] = "화";
+    
+        print $week[0];
+        print $week[1];
+    
+        $month=array(1,2,3,4,5,6,7,8,9,"월");
+        $year=[2016,2017,2018];
+    
+        $month=array();
+    
+        $member["name"]="곽ㅇㅊ";
+        $member["age"]=3;
+        $member["height"]=150;
+        print $member['age'];
+        print "<pre>";
+        print_r ($member);
+    
+        ?>
+        </ul>
+        <div class='gugudan'>
+           <?php
+            for ($i=1;$i<=9;$i++) {
+                if ($i%2==1) {
+                    print ("<p class='odddan'>");
+                    for ($j=1; $j<=9;$j++) {
+                        print ($i." X ".$j." = ".$i*$j."<br>");
+                    }
+                    print("</p>");
+                } else {
+                    for ($j=1; $j<=9;$j++) {
+                         print ($i." X ".$j." = ".$i*$j."<br>");
+                    }
+                }
+                print "<br>";
+            }
+            ?>
+        </div>
+    </body>
 </html>
