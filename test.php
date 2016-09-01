dbeb4e002fcaaa135bb69d4d925501895ff92287
dlvmwpr <dlvmwpr>
2016-09-01 10:40:36 +0900

    상수

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-    phpinfo();
+//    phpinfo();
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -10,13 +10,21 @@
 <body>
 <ul>
     <li>1번</li>
-    <li>
+    <li>2번</li>
     <?php
         print "ㅁㅇㄻㄴㅇㄻㄴ";
     ?>
+    <li>3번째 예제
+    <?php
+        print PHP_VERSION;
+        print PHP_OS;
+        define ("HELLO", "안녕하세요");
+        print HELLO."<BR>";
+        define ("_18", "Eighteen");
+        print _18."<BR>";
+        print __DIR__;
+    ?>
     </li>
-    <li>2번</li>
-    <li>3번</li>
     <li>4번</li>
 </ul>
 </body>
